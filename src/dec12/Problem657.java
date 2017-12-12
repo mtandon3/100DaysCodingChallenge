147520cb7bfcfc533b07c6d5a4ec2a8f292fec3b
mtandon3 <manishtandon11@gmail.com>
2017-12-12 16:33:24 -0700

    657

--- src/dec12/Problem657.java
+++ src/dec12/Problem657.java
@@ -1,10 +1,35 @@
 package dec12;
 
 public class Problem657 {
-
+	public static boolean judgeCircle(String moves) {
+        char[] chMoves=moves.toCharArray();
+        int x=0;
+        int y=0;
+        
+        for(int i=0;i<chMoves.length;i++){
+            if(chMoves[i]=='U') {
+            	y+=1;
+            }
+            if(chMoves[i]=='D') {
+            	y-=1;
+            }
+            if(chMoves[i]=='L') {
+            	x-=1;
+            }
+            if(chMoves[i]=='R') {
+            	x+=1;
+            }
+        }
+        if(x==0 && y==0) {
+        	return true;
+        }else {
+        	return false;
+        }
+    }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println(judgeCircle("UD"));
+		
 	}
 
 }
