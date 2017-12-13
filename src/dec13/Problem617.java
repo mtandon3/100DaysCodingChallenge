500fd3df7957e89ce1620e670112350547ff030f
manish <manishtandon11@gmail.com>
2017-12-13 13:10:27 -0700

    617 leetcode

---
+++ src/dec13/Problem617.java
@@ -0,0 +1,23 @@
+package dec13;
+
+public class Problem617 {
+	 //TreeNode t3=new TreeNode(t1.val+t2.val);
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        
+        if(t1==null){
+            return t2;
+        }
+        if(t2==null)
+            return t1;
+        
+        t1.val=t1.val+t2.val;
+        t1.left=mergeTrees(t1.left,t2.left);
+        t1.right=mergeTrees(t1.right,t2.right);
+        return t1;
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
